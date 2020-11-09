dfa448af58f61ec1726b9f0e00432c6fc8cc8f54
Rich Boothe <richardjboothe@gmail.com>
2020-11-09 15:33:37 -0600

    remove old solution

--- addMinutes.js
+++ addMinutes.js
@@ -1,57 +1,82 @@
-const buildMinutes = (originalMinutes = 0, additionalMinutes = 0) => {
-    const initialMinutes = originalMinutes + additionalMinutes;
-    const hoursToPass = Math.floor(initialMinutes/60);
-    const minutesToPass = initialMinutes % 60;
-    return [hoursToPass, minutesToPass];
+const breakdownTimeString = timeString => {
+    const timeStringLength = timeString.length;
+    const meridiem = timeString.substring(timeStringLength - 2);
+    const hour = timeStringLength === 8 ? timeString.substring(0, 2) : timeString.substring(0, 1);
+    const minutes = timeStringLength === 8 ? timeString.substring(3, 5) : timeString.substring(2, 4);
+    
+    return { hour, minutes, meridiem };
+};
+
+const convertFrom12To24 = (hours, meridiem) => {
+    const intHours = Number(hours);
+    if (intHours === 12) {
+        if (meridiem === 'AM') return 0;
+        return 12;
+    }
+
+    if (meridiem === 'PM') return intHours + 12;
+    return intHours;
+};
+
+const convertFrom24To12 = hours => {
+    if (hours === 0) return { finalHours: '12', finalMeridiem: 'AM' };
+    if (hours < 12) return { finalHours: `${hours}`, finalMeridiem: 'AM' };
+    if (hours === 12) return { finalHours: `${hours}`, finalMeridiem: 'PM'};
+    return { finalHours: `${hours - 12}`, finalMeridiem: 'PM' };
+};
+
+const setMinutes = (originalMinutes, minutesToAdd) => {
+    const intMinutes = Number(originalMinutes);
+    const totalMinutes = intMinutes + minutesToAdd;
+    const hoursToAdd = Math.floor(totalMinutes / 60);
+    const modulatedMinutes = totalMinutes % 60;
+    const finalMinutes = modulatedMinutes < 10 ? `0${modulatedMinutes}` : `${modulatedMinutes}`;
+    return { hoursToAdd, finalMinutes };
 }
 
-const buildHours = (originalHours = 0, additionalHours = 0) => originalHours + additionalHours;
+const setFinalHours = (militaryHours, hoursToAdd) => {
+    if (!hoursToAdd) return militaryHours;
+
+    const totalHours = militaryHours + hoursToAdd;
+    return totalHours <= 23 ? totalHours : (totalHours % 24);
+}
 
 const addMinutes = (timeString = "12:00 AM", minutesToAdd = 0) => {
     if (!minutesToAdd) return timeString;
 
-    const timeStringLength = timeString.length;
-    let meridiem = timeString.substring(timeStringLength - 2);
-    const timeToModify = timeStringLength === 7 
-        ? timeString.substring(0, 4).split(':').map(string => Number(string))
-        : timeString.substring(0,5).split(':').map(string => Number(string));
+    const timeStringElements = breakdownTimeString(timeString);
+    // set time from AM/PM to military time
+    const militaryHours = convertFrom12To24(timeStringElements.hour, timeStringElements.meridiem);
+    // find value of total minutes broken into {hoursToAdd, finalMinutes}
+    const { hoursToAdd, finalMinutes } = setMinutes(timeStringElements.minutes, minutesToAdd);
+    // resolve hours after adding any additional hours
+    const computedHours = setFinalHours(militaryHours, hoursToAdd);
+    const {finalHours, finalMeridiem} = convertFrom24To12(computedHours);
 
-    if (meridiem === 'PM') {
-        timeToModify[0] = timeToModify[0] < 12 
-            ? timeToModify[0] += 12
-            : timeToModify[0] = 0;    
-    }
-    const minuteValue = buildMinutes(timeToModify[1], minutesToAdd);
-    timeToModify[0] = buildHours(timeToModify[0], minuteValue[0]);
-    timeToModify[1] = minuteValue[1];
-    console.log({timeToModify});
-    let remainingHours = timeToModify[0] % 24;    
-    let meridiemFlips = Math.floor(timeToModify[0] / 24);
-
-    while(meridiemFlips > 0) {
-        meridiem = meridiem === 'PM' ? 'AM' : 'PM';
-        meridiemFlips--;
-    };
-
-    if (remainingHours > 12) {
-        remainingHours -= 12;
-        meridiem = meridiem === 'PM' ? 'AM' : 'PM';
-    } else if (remainingHours === 0) {
-        remainingHours = 12;
-        meridiem = 'AM';
-    }
-    if (timeToModify[1] === 0) {
-        timeToModify[1] = '00';
-    }
-    return `${remainingHours}:${timeToModify[1]} ${meridiem}`;
+    return `${finalHours}:${finalMinutes} ${finalMeridiem}`;
 }
 
+// for(let i = 1; i < 13; i++) {
+//     for(let j = 0; j <= 1; j++) {
+//         j === 0 ? console.log('AM:', convertFrom12To24(i, 'AM'))
+//             : console.log('PM:', convertFrom12To24(i, 'PM'));
+//     }
+// }
+
+// for(let militaryHrs = 0; militaryHrs < 24; militaryHrs++) {
+//     for (let addedHours = 0; addedHours < 24; addedHours++) {
+//         console.log({militaryHrs, addedHours, output: setFinalHours(militaryHrs, addedHours)});
+//     }
+// }
+
+// for(let hours = 0; hours < 24; hours++) {
+//     console.log({hours: hours, output: convertFrom24To12(hours)});
+// }
 
 console.log(addMinutes('9:00 PM', 600)); //=> 7:00 AM
 console.log(addMinutes('9:00 PM', 2880)); // => 9:00 AM
 console.log(addMinutes('9:00 PM', 4320)); // => 9:00 PM
 console.log(addMinutes('9:00 PM', 5760)); // => 9:00 PM
-console.log(2880 / 60 / 24);
 console.log(addMinutes('9:13 PM', 0)) //=> 9:13 PM
 console.log(addMinutes('9:13 PM', 60)) //=> 10:13 PM
 console.log(addMinutes('9:13 PM', 600)) //=> 7:13 AM
